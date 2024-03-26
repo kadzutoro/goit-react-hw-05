e64f06ec12427a8d10643b41980fcb38d5a33a41
kadzutoro <nikitashpir132@gmail.com>
2024-03-27 00:15:08 +0200

    u

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+   <div>
+    <routes>
+      <route path='/' element={< HomePage />} />
+    </routes>
+   </div>
+  )
+}
+
+export default App
